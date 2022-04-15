919d99cf05d99c23dffb1ce1407fa84b30e09eba
Target Computer <n/a>
2022-04-15 10:09:22 -0500

    Added more known devices.

--- serial_communication/list_usb_serial_ports.ps1
+++ serial_communication/list_usb_serial_ports.ps1
@@ -5,6 +5,7 @@ echo "Known devices for the E1039 target slow control:"
 echo "  COM 8  FTDIBUS VID_0403 PID_6001 AB0MSAYFA  MaxiGauge"
 echo "  COM 5  FTDIBUS VID_0403 PID_6001 A10KNO4KA  MKS 670"
 echo "  COM 9  FTDIBUS VID_0403 PID_6001 AB0MSAYBA  Run Valve"
+echo "  COM10  FTDIBUS\VID_0403+PID_6001+A10KNNHMA  Microwave Control Box #1"
 echo "  COM11  FTDIBUS VID_0403 PID_6001 AB0MSGBOA  AMI 1700"
 echo "  COM13  FTDIBUS VID_0403 PID_6001 A10KNMWOA  THCD-400"
 echo "  COM14  FTDIBUS VID_0403 PID_6001 AB0MSNGDA  MKS 946"
@@ -12,11 +13,15 @@ echo "  COM16  FTDIBUS VID_0403 PID_6015 DAZ5STTNA  Lifter Potentiometer Power S
 echo "  COM17  FTDIBUS VID_0403 PID_6001 AK05THHHA  ??"
 echo "  COM21  FTDIBUS VID_0403 PID_6001 A103LMKMA  Oxford ITM 10"
 echo "  COM22  FTDIBUS VID_0403 PID_6001 AB0MSCGVA  Bypass Valve"
+echo "  COM23  FTDIBUS\VID_0403+PID_6001+A10LG3PJA  Microwave Control Box #2"
+echo "  COM24  FTDIBUS\VID_0403+PID_6015+DA5PDJH9A  Gas Analyzer BGA244"
+echo "  COM25  USB\VID_2341&PID_0043\950323035373517042F0  Roots-pump LCW Arduino"
+
 echo ""
 echo "Available devices at present:"
 
 while ($true) {
-  Get-WMIObject Win32_PnPEntity | where {$_.Name -like "USB Serial Port*"} | Format-Table Name, PNPDeviceID
+  Get-WMIObject Win32_PnPEntity | where {$_.Name -like "USB Serial *COM*"} | Format-Table Name, PNPDeviceID
   Write-Host "Press Control-C to quit.  " -NoNewLine
   Pause
 }
